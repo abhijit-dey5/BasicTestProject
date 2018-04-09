abd1cab86b85fac9830741e6cc07d761895c0f0e
Abhijit Dey <939118@uk.ema.ad.pwcinternal.com>
2018-04-09 07:17:44 +0100

    deleted non-required modules

--- modules/pwc_disable_ssl_enable_tls_protocols/spec/classes/init_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-describe 'pwc_disable_ssl_enable_tls_protocols' do
-  context 'with default values for all parameters' do
-    it { should contain_class('pwc_disable_ssl_enable_tls_protocols') }
-  end
-end
--- modules/pwc_http_to_https_redirect/spec/classes/init_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-describe 'pwc_http_to_https_redirect' do
-  context 'with default values for all parameters' do
-    it { should contain_class('pwc_http_to_https_redirect') }
-  end
-end
