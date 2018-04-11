e71fef2cb3aecb669e551b7731713aa46ed8a18a
abhijit-dey5 <37848197+abhijit-dey5@users.noreply.github.com>
2018-04-11 08:56:57 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,14 @@
+node default {
+}
 
+node 'ukgbdcpupdl002.uk.ema.pwcinternal.com' {
+}
+
+node 'ukgbdcmdfdw604.uk.ema.ad.pwcinternal.com' {
+}
+
+node 'ukgbdcmdfdw207.uk.ema.ad.pwcinternal.com' {
+}
+
+node 'ukgbdcappdw103.uk.ema.ad.pwcinternal.com' {
+}
